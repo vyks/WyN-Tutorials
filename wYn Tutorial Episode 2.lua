455051a40eea001e8b84ea264296c65ae6f6c604
Griffin // vykzs <89820610+vyks@users.noreply.github.com>
2021-09-01 19:14:55 -0400

    Finished Video

--- wYn Tutorial Episode 2.lua
+++ wYn Tutorial Episode 2.lua
@@ -1,29 +1,86 @@
 --[[EPISODE TWO WYN TUTORIAL]]--
---[[game = https://www.roblox.com/games/5098929086]]
+--[[game = game = https://www.roblox.com/games/6859865958]]
 --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 --Globals--
-_G.autotap = true
-_G.autorebirth = true
+
+getgenv().Autotap = true
+getgenv().Autovideo = true
 
 --Locals--
 
-local remotepath = game:GetService("ReplicatedStorage")
+local Replicate = game:GetService("ReplicatedStorage")
 
-for i, v in pairs(_G) do
-    print(i,v)
+--Script--
+
+function doTap()
+    spawn(function()
+        while Autotap == true do
+            Replicate.fileMade:FireServer()
+            wait()
+        end 
+    end)
 end
 
-spawn(function()
-    while _G.autotap == true do
-        remotepath.PlayerClick:FireServer(false)
-        wait()
-    end
-end)
+doTap()
 
-spawn(function()
-    while _G.autorebirth == true do
-        remotepath.PlayerRebirth:FireServer(1)
-        wait()
+function doVideo()
+    spawn(function()
+        while Autovideo == true do
+            --local args = {[1] = videoamount}--
+            Replicate.singleVideo:FireServer(workspace.Studios["Qnotic's Studio"].Items["Starter Laptop"])--(unpack(args))--
+            wait() 
+        end
+    end)
+end
+
+doVideo()
+
+function getCurrentPlayerPOS()
+    local plyer = game.Players.LocalPlayer
+    if plyr.Character then
+        return plyer.Character.HumanoidRootPart.Position
     end
-end)
-print ('hello') 
+        return false
+end
+
+
+
+
+
+
+
+
+--Teleports--
+
+function upgradeTele(placeCFrame)
+    local plyer = game.Players.LocalPlayer
+        plyer.Character.HumanoidRootPart.CFrame = placeCFrame
+end
+
+upgradeTele(game:GetService("Workspace").upgradeStore.Door.entrancePart.CFrame)
+
+function itemTele(placeCFrame)
+    local plyer = game.Players.LocalPlayer
+        plyer.Character.HumanoidRootPart.CFrame = placeCFrame
+end
+
+itemTele(game:GetService("Workspace").SHOP.enterPart.CFrame)
+
+function HouseTele(placeCFrame)
+    local plyer = game.Players.LocalPlayer
+        plyer.Character.HumanoidRootPart.CFrame = placeCFrame
+end
+
+HouseTele(game:GetService("Workspace").Studios["Qnotic's Studio"].Items.Cushion.chair.CFrame)
+Replicate.unSit:FireServer()
+Replicate.unSit:FireServer()
+
+--LEARNING PURPOSE--
+--[[
+function teleportworld(world)
+    if game:GetService("Workspace").Worlds:FindFirstChild(world)
+        teleportTO(game:GetService("Workspace").Worlds.[world].Teleport.CFrame)
+    end
+end
+
+teleportworold("Desert") ]]--
